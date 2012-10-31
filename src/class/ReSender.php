bdf5f4ec6c18c940c47e6e5c0bc03eb94f5d200c
Charles Gultom <gultom.c@gmail.com>
2012-10-31 09:15:13 +0700

    write log if no pending tickets found

--- src/class/ReSender.php
+++ src/class/ReSender.php
@@ -14,20 +14,25 @@ class ReSender {
      */
     public function resendMail(Ticket $ticket, Mail $mail) {
         $tickets = $ticket->getPendingTickets($this->limitDay);
+        $Log = new Log();
+        $Log->setLogPath(__LOG_URI_PATH__);
+        $Log->setLogDir(date('Y-m'));
+        $Log->setLogFile(date('ymd') .'.log');
+        
         if (count($tickets)) {
-            $Log = new Log();
             foreach ($tickets as $key => $value) {
                 $ticket->setId($value['ID']);
                 $mail->getMailByTicket($ticket);
                 if ($mail->resend()) {
                     $logMsg = "Re-Sending approval Email ID ". $mail->getId() ." for Ticket #". $value['TicketNo'] ." [". date('d M Y, H:i:s') ."]\n";
-                    $Log->setLogPath(__LOG_URI_PATH__);
-                    $Log->setLogDir(date('Y-m'));
-                    $Log->setLogFile(date('ymd') .'.log');
-                    @$Log->writeLog($logMsg, 'a+');
                 }
             }
         }
+        else {
+            $logMsg = "Re-Sender had been executed on ". date('d M Y, H:i:s') .", but no pending tickets approval found";
+        }
+        
+        @$Log->writeLog($logMsg, 'a+');
     }
 }
 ?>
